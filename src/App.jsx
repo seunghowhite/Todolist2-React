1f921fce08a539f4aa06ede7069aa535bf701db5
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-03-07 00:51:39 +0900

    버튼 기능 초안 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { addtodo } from './redux/modules/todolists';
+import { addtodo, deletetodo, toggletodo } from './redux/modules/todolists';
 
 
 
 
 const App = () => {
   const [title, setTitle] = useState('')//로컬 state값 
+  const [content, setContent] = useState('')
   const todolists = useSelector((state) => state.storetodolists.todolists)//배열 key값 잘 작성 해야함
   const dispatch = useDispatch();
 
@@ -16,20 +17,43 @@ const App = () => {
     e.preventDefault();//이벤트 억제 중요
 
 
-    if (title === "") {//입력값 받기
+    if (title === "" || content === "") {//입력값 받기
       alert('값을 입력하세요')
       return
     };
 
+
     dispatch(//모듈에있는 함수를 가지고와서 action객체를 던진다.
       addtodo({
         id: todolists.length + 1,
-        title: title //title값 받아서 전해준다
+        title: title, //title값 받아서 전해준다
+        content: content,
+        isDone: false,
       })
     )
 
     setTitle('')//버튼 클릭후 초기화
+    setContent('')//버튼 클릭후 초기화
   }
+
+
+  //삭재하기
+
+  const deletTodo = (id) => {
+    dispatch(deletetodo(//그냥id만 보냄
+      id
+    ))
+  }
+
+  //완료
+  const toggleTodo = (id) => {
+    dispatch(toggletodo(
+      id
+    ))
+  }
+
+
+
   return (
     <>
       {/*투두리스트  */}
@@ -39,18 +63,29 @@ const App = () => {
 
       {/* 제출하는곳 */}
       <form onSubmit={sumbitTitle}>
-
         <input value={title} onChange={(event) => setTitle(event.target.value)}></input>
-        {title}
-
+        <input value={content} onChange={(event) => setContent(event.target.value)}></input>
         <button >추가</button>
       </form>
-
       {/* 보여주는곳 */}
-      <div>
-        {todolists.map((list) => {
+      <div style={{ border: '1px solid black' }}>
+        {todolists.filter((list) => list.isDone === false).map((list) => {
           return (
-            <div key={list.id}>{list.title}</div>
+            <div key={list.id}>제목:{list.title}내용:{list.content}
+              <button on onClick={() => deletTodo(list.id)}>삭재</button>
+              <button on onClick={() => toggleTodo(list.id)}>완료</button>
+            </div>
+          )
+        })}
+      </div>
+
+      <div style={{ border: '1px solid black' }}>
+        {todolists.filter((list) => list.isDone === true).map((list) => {
+          return (
+            <div key={list.id}>제목:{list.title}내용:{list.content}
+              <button on onClick={() => deletTodo(list.id)}>삭재</button>
+              <button on onClick={() => toggleTodo(list.id)}>취소</button>
+            </div>
           )
         })}
       </div>
--- src/redux/modules/todolists.js
+++ src/redux/modules/todolists.js
@@ -1,10 +1,19 @@
 //ActionValue
 const ADD_TODO = 'ADDTODO'
+const DELETE_TODO = 'DELETETODO'
+const TOGGLE_TODO = 'TOGGLETODO'
+
 //ActionCreator
 export const addtodo = (payload) => {
-  return { type: ADD_TODO, payload: payload }
+  return { type: ADD_TODO, payload }
+}
+export const deletetodo = (payload) => {
+  return { type: DELETE_TODO, payload }
 }
 
+export const toggletodo = (payload) => {
+  return { type: TOGGLE_TODO, payload }
+}
 
 //기존값
 const initialSate = {
@@ -12,11 +21,14 @@ const initialSate = {
     {
       id: 1,
       title: "리액트 강의보기",
-
+      content: "리액트내용",
+      isDone: false,
     },
     {
       id: 2,
       title: "점심 먹기",
+      content: "짜장면 탕수욕",
+      isDone: true,
 
     },
   ],
@@ -30,6 +42,21 @@ const todolists = (state = initialSate, action) => {
         ...state,
         todolists: [...state.todolists, action.payload]
       };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todolists: [...state.todolists.filter((item) => item.id !== action.payload)]//필터
+      }
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todolists: state.todolists.map((item) => ({
+          ...item, isDone: item.id === action.payload ? !item.isDone : item.isDone,
+        }))
+      };
+
+
     default:
       return state
   }
